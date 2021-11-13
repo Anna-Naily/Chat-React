635b3794871fe6cca5f8d9a15332ed94af31d240
Anna_Umanskaya <zareth2_@mail.ru>
2021-11-13 11:59:45 +0300

    lesson 5

--- src/components/Profile/Profile.js
+++ src/components/Profile/Profile.js
@@ -1,8 +1,20 @@
 import React from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { toggleCheckbox } from "../../store/profile/actions";
+import "./Profile.css";
+
 export const Profile = () => {
+  const checkboxValue = useSelector(state => state.checkbox);
+  const name = useSelector(state => state.name);
+  const dispatch = useDispatch();
+  const handleChange = () => {
+    dispatch(toggleCheckbox);
+  };
+
   return (
     <div className="profile-block">
-      <h1>Мой профиль</h1>
+      <input type="checkbox" checked={checkboxValue} onChange={handleChange} />
+      <h1 className="profile-block__name">{name}</h1>
     </div>
   );
 };
